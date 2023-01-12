dbff250ad08e833f16c17a9b8982dfe193054a25
Bruna Stephane da Silva <brunastephanedasilva@gmail.com>
2023-01-12 16:20:21 +0000

    Created NavBar Component

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -3,34 +3,32 @@ import { Button, Form, NavDropdown } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import "./NavBar.scss";
 
 export default function NavBar() {
   return (
-<Navbar bg="light" expand="lg">
+    <Navbar expand="lg"  variant="dark" className="customNavbar">
       <Container fluid>
-        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
             className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
             navbarScroll
           >
+            <Container>
+              <Navbar.Brand href="#home">
+                <img
+                  src="images/logo-no-background.png"
+                  width="150"
+                  className="d-inline-block align-top"
+                  alt="React Bootstrap logo"
+                />
+              </Navbar.Brand>
+            </Container>
             <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
-            <NavDropdown title="Link" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Something else here
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="#" disabled>
-              Link
-            </Nav.Link>
+            <Nav.Link href="#action2">Resources</Nav.Link>
+            <Nav.Link href="#action2">Blog</Nav.Link>
+            <Nav.Link href="#action2">Contact</Nav.Link>
           </Nav>
           <Form className="d-flex">
             <Form.Control
